4ed2d021118c52b900aa51e8d62667e8d2b15281
Keshav_Verma <144476724+keshav-v2004@users.noreply.github.com>
2026-01-03 14:09:52 +0530

    Time: 3 ms (22.68%) | Memory: 43 MB (22.60%) - LeetSync

--- 394-decode-string/decode-string.java
+++ 394-decode-string/decode-string.java
@@ -1,48 +1,51 @@
-import java.util.Stack;
-
 class Solution {
     public int index = 0;
 
     public String decodeString(String s) {
+
         Stack<Integer> stack = new Stack<>();
-        return helper(s, stack).toString();
+
+        return helper(s,stack).toString();
+
     }
 
-    public StringBuilder helper(String s, Stack<Integer> stack) {
+    public StringBuilder helper(String s , Stack<Integer> stack){
+
         StringBuilder temp = new StringBuilder();
 
-        while (index < s.length()) {
+        while(index < s.length()){
+
             char ch = s.charAt(index);
+            if(ch=='['){
+                index++;
+                temp.append(helper(s,stack));
+            }
+            else if(ch==']'){
+                index++;
+                StringBuilder rep = new StringBuilder();
+                int count = stack.pop();
+                for(int i = 0 ; i <count ; i++){
+                    rep.append(temp);
+                }
+                return rep;
 
-            if (Character.isDigit(ch)) {
-
+            }
+            else if(Character.isDigit(ch)){
                 String freq = "";
-                while (index < s.length() && Character.isDigit(s.charAt(index))) {
+                while(index < s.length() && Character.isDigit(s.charAt(index))){
                     freq += s.charAt(index);
                     index++;
                 }
                 stack.push(Integer.parseInt(freq));
 
-            } else if (ch == '[') {
-                index++; 
-                
-                temp.append(helper(s, stack));
-
-            } else if (ch == ']') {
-                index++; 
-                int count = stack.pop(); 
-                StringBuilder rep = new StringBuilder();
-                for (int i = 0; i < count; i++) {
-                    rep.append(temp);
-                }
-                return rep; 
-
-            } else { 
-                temp.append(ch);
-                index++;
             }
-        }
-
+            else{
+                    temp.append(s.charAt(index));
+                    index++;
+            }
+        }  
         return temp;
+
+
     }
 }
