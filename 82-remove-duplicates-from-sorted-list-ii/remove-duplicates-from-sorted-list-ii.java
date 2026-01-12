ae07e70393dddced19c8b1c855696af1a784dcc0
Keshav_Verma <144476724+keshav-v2004@users.noreply.github.com>
2026-01-12 16:24:58 +0530

    Time: 0 ms (100.00%) | Memory: 45.3 MB (11.16%) - LeetSync

--- 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.java
+++ 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.java
@@ -1,52 +1,32 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-
-class Solution{
+class Solution {
     public ListNode deleteDuplicates(ListNode head) {
-    if(head == null || head.next == null) return head;
+        ListNode mover = head;
+        ListNode newHead = null;
+        ListNode newMover = null;
 
-    HashMap<Integer , Integer> map = new HashMap<>();
-    ListNode mover = head;
+        while (mover != null) {
 
-    while(mover != null){
-        map.put(mover.val , map.getOrDefault(mover.val , 0) + 1);
-        mover = mover.next;
-    }
+            if (mover.next != null && mover.val == mover.next.val) {
+                int val = mover.val;
+                while (mover != null && mover.val == val) {
+                    mover = mover.next;
+                }
+            } 
+            else {
 
-    ListNode newHead = null;
-    mover = head;
+                if (newHead == null) {
+                    newHead = mover;
+                    newMover = mover;
+                } else {
+                    newMover.next = mover;
+                    newMover = newMover.next;
+                }
 
-    while(mover != null){
-        if(map.get(mover.val) == 1){
-            newHead = mover;
-            mover = mover.next;
-            break;
+                mover = mover.next;
+                newMover.next = null; 
+            }
         }
-        mover = mover.next;
+
+        return newHead;
     }
-
-    if (newHead == null) return null; 
-
-    ListNode newMover = newHead;
-
-    while(mover != null){
-        if(map.get(mover.val) == 1){
-            newMover.next = mover;
-            newMover = newMover.next;
-        }
-        mover = mover.next;
-    }
-
-    newMover.next = null; 
-    return newHead;
-}
-
 }
