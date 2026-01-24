a5f98487bd4bdf97447c7f1492cc4329b53fca5e
Keshav_Verma <144476724+keshav-v2004@users.noreply.github.com>
2026-01-24 16:35:00 +0530

    Time: 7 ms (42.38%) | Memory: 51.5 MB (5.44%) - LeetSync

---
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.java
@@ -0,0 +1,49 @@
+class Solution {
+
+    Integer[][][] dp;
+
+    public int maxProfit( int k , int[] prices) {
+        int n = prices.length;
+        dp = new Integer[n][2][k+1];   // index, holding(0/1), k = k+1
+        return helper(prices, 0, 1, k);  // start free, 2 transactions
+    }
+
+    // holding = 1 -> free to buy
+    // holding = 0 -> stock in hand
+    public int helper(int[] prices, int index, int holding, int k) {
+
+        if (index == prices.length || k == 0)
+            return 0;
+
+        if (dp[index][holding][k] != null)
+            return dp[index][holding][k];
+
+        int price = prices[index];
+        int profit;
+
+        if (holding == 1) {
+            // free to buy
+
+            // skip
+            int p1 = helper(prices, index + 1, 1, k);
+
+            // buy
+            int p2 = -price + helper(prices, index + 1, 0, k);
+
+            profit = Math.max(p1, p2);
+
+        } else {
+            // holding stock
+
+            // skip
+            int p1 = helper(prices, index + 1, 0, k);
+
+            // sell (consume 1 transaction)
+            int p2 = price + helper(prices, index + 1, 1, k - 1);
+
+            profit = Math.max(p1, p2);
+        }
+
+        return dp[index][holding][k] = profit;
+    }
+}
