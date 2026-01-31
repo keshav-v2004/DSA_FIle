e25442d8c9ed35d61adcffea79eb598ba8528bc0
Keshav_Verma <144476724+keshav-v2004@users.noreply.github.com>
2026-01-31 15:18:43 +0530

    Time: 29 ms (31.52%) | Memory: 47.8 MB (27.02%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.java
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -1,38 +1,60 @@
 class Solution {
     public int minInsertions(String s) {
-        int start = 0;
-        int end = s.length()-1;
+        return s.length() - longestPalindromeSubseq(s);
 
-        int n = s.length();
-        int[][] dp = new int[n][n];
-        for(int i = 0 ; i < n ; i++){
-            Arrays.fill(dp[i] , -1);
-        }
-
-        return helper(s , start , end  ,dp);
     }
 
-    public int helper(String s , int i , int j , int[][] dp){
+    public int longestPalindromeSubseq(String s) {
+        String r = new StringBuilder(s).reverse().toString();
 
+        int i = s.length();
+        int j = s.length();
 
-        if(i>=j) return 0;
+        int[][] dp = new int[i + 1][i + 1];
 
-        if(dp[i][j] != -1) return dp[i][j];
+        for (int k = 0; k < i + 1; k++) {
+            dp[0][k] = 0;
+            dp[k][0] = 0;
 
-        char ch1 = s.charAt(i);
-        char ch2 = s.charAt(j);
-
-        int answer = (int) 1e9;
-
-        if(ch1==ch2){
-            answer = helper(s , i+1 , j-1 , dp);
-        }
-        else{
-            int insertAtLeft = 1 + helper(s , i , j-1 , dp);
-            int insertAtRight = 1 + helper(s , i+1 , j , dp);
-            answer = Math.min(insertAtLeft , insertAtRight);
         }
 
-        return dp[i][j] = answer;
+        for (int k = 1; k < i + 1; k++) {
+            for (int l = 1; l < j + 1; l++) {
+                char ch1 = s.charAt(k - 1);
+                char ch2 = r.charAt(l - 1);
+
+                if (ch1 == ch2) {
+                    dp[k][l] = 1 + dp[k - 1][l - 1];
+                } else {
+                    dp[k][l] = Math.max(dp[k - 1][l], dp[k][l - 1]);
+                }
+            }
+        }
+
+        StringBuilder answer = new StringBuilder();
+        i = s.length();
+        j = s.length();
+
+        while (i > 0 && j > 0) {
+            int cur = dp[i][j];
+
+            if (cur > Math.max(dp[i - 1][j], dp[i][j - 1])) {
+                answer.append(s.charAt(i - 1));
+                i--;
+                j--;
+            } else {
+                if (cur == dp[i - 1][j]) {
+                    i--;
+                } else {
+                    j--;
+                }
+            }
+        }
+
+        // longest palindromic subsequence
+        System.out.println(answer.reverse().toString());
+
+        return answer.length();
     }
+
 }
