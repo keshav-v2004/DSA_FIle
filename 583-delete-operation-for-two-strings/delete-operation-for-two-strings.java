e878d18db5ddd4249649e73d2ce22b10b53ee300
Keshav_Verma <144476724+keshav-v2004@users.noreply.github.com>
2026-01-31 15:28:26 +0530

    Time: 7 ms (71.02%) | Memory: 46.4 MB (62.66%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.java
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.java
@@ -3,35 +3,56 @@ class Solution {
         int i = word1.length();
         int j = word2.length();
 
-        int[][] dp = new int[i][j];
-        for(int index = 0 ; index < dp.length ; index++){
-            Arrays.fill(dp[index] ,-1);
+        int[][] dp = new int[i + 1][j + 1];
+
+        for (int k = 0; k < dp[0].length; k++) {
+            dp[0][k] = k;
+        }
+        for (int k = 0; k < dp.length; k++) {
+            dp[k][0] = k;
+        }
+        dp[0][0] = 0;
+        for (int k = 1; k < i + 1; k++) {
+            for (int l = 1; l < j + 1; l++) {
+                char ch1 = word1.charAt(k - 1);
+                char ch2 = word2.charAt(l - 1);
+
+                
+                if (ch1 == ch2) {
+                    dp[k][l] = dp[k-1][l-1];
+                } else {
+                    dp[k][l] = 1 + Math.min(
+                            dp[k-1][l], dp[k][l-1]);
+                }
+            }
         }
 
-        return helper(word1 , word2 , i-1 ,  j-1 , dp);
+        return dp[i][j];
+
     }
 
-    public int helper(String word1 , String word2 , int i , int j , int[][] dp){
-        
-        if(i < 0 || j < 0) {
-            if(i < 0 && j < 0) return 0;
-            else if(i < 0) return j+1;
-            return i+1;
+    public int helper(String word1, String word2, int i, int j, int[][] dp) {
+
+        if (i == 0 || j == 0) {
+            if (i == 0 && j == 0)
+                return 0;
+            else if (i == 0)
+                return j;
+            return i;
         }
 
-        if(dp[i][j] != -1) return dp[i][j];
+        if (dp[i][j] != -1)
+            return dp[i][j];
 
-        char ch1 = word1.charAt(i);
-        char ch2 = word2.charAt(j);
+        char ch1 = word1.charAt(i - 1);
+        char ch2 = word2.charAt(j - 1);
 
         int answer = 0;
-        if(ch1==ch2){
-            answer = helper(word1 , word2 , i-1 , j-1 , dp);
-        }
-        else{
+        if (ch1 == ch2) {
+            answer = helper(word1, word2, i - 1, j - 1, dp);
+        } else {
             answer = 1 + Math.min(
-                helper(word1 , word2 , i-1 , j , dp) , helper(word1 , word2 , i , j-1 , dp)
-            );
+                    helper(word1, word2, i - 1, j, dp), helper(word1, word2, i, j - 1, dp));
         }
 
         return dp[i][j] = answer;
