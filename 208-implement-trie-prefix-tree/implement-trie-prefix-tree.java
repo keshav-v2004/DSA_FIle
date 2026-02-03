bc3b044f2ed18a972526c21d0da6b16e0298d6ff
Keshav_Verma <144476724+keshav-v2004@users.noreply.github.com>
2026-02-03 20:58:52 +0530

    Time: 37 ms (36.93%) | Memory: 62.2 MB (52.17%) - LeetSync

--- 208-implement-trie-prefix-tree/implement-trie-prefix-tree.java
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.java
@@ -1,28 +1,86 @@
+class Node{
+    Node[] nodes= new Node[26];
+    boolean isEnd = false;
+
+    boolean containsKey(char ch){
+        if(nodes[ch - 'a'] != null){
+            return true;
+        }
+        return false;
+
+    }
+
+    void setEnd(){
+        isEnd = true;
+    }
+
+    void put(char ch , Node mover){
+        nodes[ch - 'a'] = mover;
+    }
+
+    Node get(char ch){
+        return nodes[ch-'a'];
+    }
+
+    Node(){}
+
+}
+
 class Trie {
-    Set<String> set;
+    Node root;
 
     public Trie() {
-        set = new HashSet<>();
+        root = new Node();
 
     }
     
     public void insert(String word) {
-        set.add(word);
+        Node mover = root;
+        int n = word.length();
+        for(int i = 0 ; i < n ; i++){
+            
+            if(!mover.containsKey(word.charAt(i))){
+                mover.put(word.charAt(i) , new Node());
+            }
+            mover = mover.get(word.charAt(i));
+        }
+        mover.setEnd();
     }
     
     public boolean search(String word) {
-        return set.contains(word);
+        Node mover = root;
+        int n = word.length();
+
+        for(int i = 0 ; i < n ; i++){
+            char ch = word.charAt(i);
+
+            if(!mover.containsKey(ch)){
+                return false;
+            }
+
+            mover = mover.get(ch);
+        }
+        if(mover.isEnd==false) return false;
+        return true;
+
     }
     
     public boolean startsWith(String prefix) {
-        for(String elem : set){
-            if(elem.startsWith(prefix)){
-                return true;
+        Node mover = root;
+
+        int n = prefix.length();
+        for(int i = 0 ; i < n ; i++){
+            char ch = prefix.charAt(i);
+
+            if(!mover.containsKey(ch)){
+                return false;
             }
+            mover = mover.get(ch);
         }
-        return false;
+        return true;  
     }
 }
+
 
 /**
  * Your Trie object will be instantiated and called as such:
