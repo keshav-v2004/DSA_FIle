f7cf7594bd81cc7bd9d7573e4194ad3c301af907
Keshav_Verma <144476724+keshav-v2004@users.noreply.github.com>
2026-03-02 13:10:18 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (75.45%) - LeetSync

--- 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.java
+++ 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.java
@@ -1,59 +1,25 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    public int index = 0;
+    int index = 0;
 
     public TreeNode bstFromPreorder(int[] preorder) {
-
-        // Preorder --> root left right
-        // inorder -->  left root right
-
-        int[] inorder = Arrays.copyOf(preorder, preorder.length);
-        Arrays.sort(inorder);
-
-        int n = inorder.length;
-
-        return helper(0, n - 1, inorder, preorder);
-
+        return build(preorder, Integer.MIN_VALUE, Integer.MAX_VALUE);
     }
 
-    public TreeNode helper(int l, int r, int[] inorder, int[] preorder) {
-        if (l > r)
+    private TreeNode build(int[] preorder, int min, int max) {
+        if (index >= preorder.length)
             return null;
 
-        int rootVal = preorder[index++];
-        int rootIndex = findRootIndex(inorder, rootVal);
+        int val = preorder[index];
 
-        TreeNode root = new TreeNode(rootVal);
-        root.left = helper(l, rootIndex - 1, inorder, preorder);
-        root.right = helper(rootIndex + 1, r, inorder, preorder);
+        if (val < min || val > max)
+            return null;
+
+        index++;
+
+        TreeNode root = new TreeNode(val);
+        root.left = build(preorder, min, val);
+        root.right = build(preorder, val, max);
 
         return root;
-
-    }
-
-    public int findRootIndex(int[] inorder, int rootVal) {
-        int index = 0;
-
-        for (int elem : inorder) {
-            if (elem == rootVal) {
-                return index;
-            }
-            index++;
-        }
-        return -1;
     }
 }
