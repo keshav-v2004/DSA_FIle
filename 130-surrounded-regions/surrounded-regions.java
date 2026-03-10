b44d9c6547cc89c7e8df8d41d82fb2673c6bc323
Keshav_Verma <144476724+keshav-v2004@users.noreply.github.com>
2026-03-10 16:26:03 +0530

    Time: 2 ms (80.44%) | Memory: 47.7 MB (84.88%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -1,80 +1,76 @@
 class Solution {
     public void solve(char[][] board) {
-
+        
+        // check the terminal rows and terminal columns 
+        // terminal O's se all the saare connected O's cannot be marked
         int m = board.length;
         int n = board[0].length;
 
-        ArrayList<ArrayList<int[]>> allPossibleRegions = new ArrayList<>();
+        int[][] visited = new int[m][n];
 
-        boolean[][] visited = new boolean[m][n];
+        // check row 1
 
-        for (int i = 1; i < m - 1; i++) {
-
-            for (int j = 1; j < n - 1; j++) {
-
-                char ch = board[i][j];
-
-                if (ch == 'O' && visited[i][j]==false) {
-
-                    ArrayList<int[]> eachRegion = new ArrayList<>();
-                    dfs(i, j, visited, eachRegion , board);
-
-                    allPossibleRegions.add(eachRegion);
-
-                } else {
-                    visited[i][j] = true;
-
-                }
+        for(int j = 0 ; j < n ; j++){
+            if(board[0][j] == 'O'){
+                dfs(0,j,visited , board);
             }
         }
 
-        for (ArrayList<int[]> each : allPossibleRegions) {
-
-            boolean isPossible = true;
-
-            for (int[] elem : each) {
-                int r = elem[0];
-                int c = elem[1];
-
-                if (r == 0 || c == 0 || r == m - 1 || c == n - 1) {
-                    isPossible = false;
-                    break;
-                }
-            }
-            if (isPossible == true) {
-                for (int[] elem : each) {
-                    int r = elem[0];
-                    int c = elem[1];
-
-                    board[r][c] = 'X';
-                }
+        // check row m-1 (last)
+                for(int j = 0 ; j < n ; j++){
+            if(board[m-1][j] == 'O'){
+                dfs(m-1,j,visited , board);
             }
         }
 
+        // check column 1
+        for(int i = 0 ; i < m ; i++){
+            if(board[i][0] == 'O'){
+                dfs(i,0,visited , board);
+            }
+        }
+
+        // check last column (n-1)
+                for(int i = 0 ; i < m ; i++){
+            if(board[i][n-1] == 'O'){
+                dfs(i,n-1,visited , board);
+            }
+        }
+
+        // now check for the other remaining inner elements
+
+        for(int i = 1 ; i < m-1 ; i++){
+            for(int j = 1 ; j < n-1 ; j++){
+
+                if(board[i][j] == 'O' && visited[i][j] == 0){
+                    board[i][j] = 'X';
+                }
+            }
+        }
     }
 
-    public void dfs(int i, int j, boolean[][] visited, ArrayList<int[]> each , char[][] grid) {
+    public void dfs(int i , int j , int[][] visited , char[][] grid){
 
-        visited[i][j] = true;
-        each.add(new int[] { i, j });
+        visited[i][j] = 1;
 
-        int[] dx = new int[] { -1, 0, 1, 0 };
-        int[] dy = new int[] { 0, 1, 0, -1 };
 
-        int m = visited.length;
-        int n = visited[0].length;
+        int m = grid.length;
+        int n = grid[0].length;
 
-        for (int k = 0; k < 4; k++) {
+        int[] dx = new int[]{-1,0,1,0};
+        int[] dy = new int[]{0,1,0,-1};
+
+        for(int k = 0 ; k < 4 ; k++){
+
             int newR = i + dx[k];
             int newCol = j + dy[k];
 
-            if (newR < m  && newCol < n  && newR >= 0 && newCol >= 0 && 
-            visited[newR][newCol] == false && grid[newR][newCol]=='O') {
-
-                dfs(newR, newCol, visited, each , grid);
+            if(newR < m && newCol < n && newR >= 0 && newCol >= 0 
+                && visited[newR][newCol]==0 && grid[newR][newCol] == 'O'
+            ){
+                dfs(newR , newCol , visited , grid);
             }
         }
 
     }
-
 }
