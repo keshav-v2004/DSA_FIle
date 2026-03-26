0d728775afe201335153e6ec32ac980c0f1f22ef
Keshav_Verma <144476724+keshav-v2004@users.noreply.github.com>
2026-03-26 23:10:33 +0530

    Time: 2 ms (44.76%) | Memory: 47.3 MB (67.70%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,47 +1,99 @@
 class Solution {
-    public int findCircleNum(int[][] isConnected) {
 
+    public int findCircleNum(int[][] isConnected) {
         int n = isConnected.length;
 
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        DisjointSet ds = new DisjointSet(n);
 
-        for(int i = 0 ; i < n ; i++){
-            adj.add(new ArrayList<>());
+        int count = 0;
 
-        }
-
-        for(int i = 0 ; i < n ; i++){
-            for(int j = 0 ; j < n ; j++){
-
-                if(i != j && isConnected[i][j]==1){
-                    adj.get(i).add(j);
+        // Union
+        for(int i = 0; i < n; i++){
+            for(int j = i+1; j < n; j++){
+                if(isConnected[i][j] == 1){
+                    ds.unionByRank(i, j);
                 }
             }
         }
 
-        int[] visited = new int[n];
-        int count = 0;
-
-        for(int i = 0 ; i < n ; i++){
-            if(visited[i]==0){
+        // Count components
+        for(int i = 0; i < n; i++){
+            if(ds.findParent(i) == i){
                 count++;
-                dfs(i , adj , visited);
             }
         }
+
         return count;
     }
+}
 
-    public void dfs(int cur, ArrayList<ArrayList<Integer>> adj,
-            int[] visited) {
-
-        visited[cur] = 1;
+class DisjointSet{
+    
+    int n;
+    
+    int[] parent;
+    int[] rank;
+    int[] size;
+    
+    DisjointSet(int n){
         
-
-        for (int elem : adj.get(cur)) {
-            if (visited[elem] == 0) {
-                dfs(elem, adj, visited);
-            }
+        this.n = n;
+        parent = new int[n+1];
+        rank = new int[n+1];
+        size = new int[n+1];
+        
+        for(int i = 1 ; i <= n ; i++){
+            parent[i] = i;
+            rank[i] = 0;
+            size[i] = 1;
+        }
+        
+    }
+    
+    void unionByRank(int u , int v){
+        int ulp_u = findParent(u);
+        int ulp_v = findParent(v);
+    
+        if(ulp_u == ulp_v) return;
+    
+        if(rank[ulp_u] < rank[ulp_v]){
+            parent[ulp_u] = ulp_v;
+        }
+        else if(rank[ulp_v] < rank[ulp_u]){
+            parent[ulp_v] = ulp_u;
+        }
+        else{
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
         }
     }
-
+    
+    void unionBySize(int u , int v){
+        int ulp_u = findParent(u);
+        int ulp_v = findParent(v);
+    
+        if(ulp_u == ulp_v) return;
+    
+        if(size[ulp_u] < size[ulp_v]){
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else{
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+    
+    int findParent(int u){
+        
+        if(parent[u] == u){
+            return u;
+            
+        }
+        
+        parent[u] = findParent(parent[u]);
+        return parent[u];
+        
+    }
+    
 }
