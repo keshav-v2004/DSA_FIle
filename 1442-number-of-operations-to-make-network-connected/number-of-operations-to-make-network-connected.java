1e03c6f5ae20fffaf277ef1e5117f484b4b25f7b
Keshav_Verma <144476724+keshav-v2004@users.noreply.github.com>
2026-03-26 23:30:03 +0530

    Time: 6 ms (63.10%) | Memory: 65.3 MB (93.61%) - LeetSync

---
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.java
@@ -0,0 +1,98 @@
+class Solution {
+    public int makeConnected(int n, int[][] connections) {
+
+        if( connections.length < n-1 ) return -1;
+
+        DisjointSet ds = new DisjointSet(n);
+
+        for(int[] edge : connections){
+            int u = edge[0];
+            int v = edge[1];
+
+            ds.unionByRank(u,v);
+        
+        }
+
+        int count = 0;
+
+        for(int i = 0 ; i < n ; i++){
+            if(ds.findParent(i) == i){
+                count++;
+            }
+        }
+        return count-1;
+
+
+    }
+}
+
+class DisjointSet{
+    
+    int n;
+    
+    int[] parent;
+    int[] rank;
+    int[] size;
+    
+    DisjointSet(int n){
+        
+        this.n = n;
+        parent = new int[n];
+        rank = new int[n];
+        size = new int[n];
+        
+        for(int i = 0 ; i < n ; i++){
+            parent[i] = i;
+            rank[i] = 0;
+            size[i] = 1;
+        }
+        
+    }
+    
+    void unionByRank(int u , int v){
+        int ulp_u = findParent(u);
+        int ulp_v = findParent(v);
+    
+        if(ulp_u == ulp_v) return;
+    
+        if(rank[ulp_u] < rank[ulp_v]){
+            parent[ulp_u] = ulp_v;
+        }
+        else if(rank[ulp_v] < rank[ulp_u]){
+            parent[ulp_v] = ulp_u;
+        }
+        else{
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+    
+    void unionBySize(int u , int v){
+        int ulp_u = findParent(u);
+        int ulp_v = findParent(v);
+    
+        if(ulp_u == ulp_v) return;
+    
+        if(size[ulp_u] < size[ulp_v]){
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else{
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+    
+    int findParent(int u){
+        
+        if(parent[u] == u){
+            return u;
+            
+        }
+        
+        parent[u] = findParent(parent[u]);
+        return parent[u];
+        
+    }
+    
+}
