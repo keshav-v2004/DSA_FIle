d07eef350339718de0a9e33e97debb081367c96f
Keshav_Verma <144476724+keshav-v2004@users.noreply.github.com>
2026-03-26 15:33:05 +0530

    Time: 52 ms (5.02%) | Memory: 151.2 MB (5.03%) - LeetSync

---
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.java
@@ -0,0 +1,70 @@
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+
+        Set<Integer> set = new HashSet<>();
+        int minOdd= Integer.MAX_VALUE;
+        int minEven = Integer.MAX_VALUE;
+
+        for (int elem : nums1) {
+            set.add(elem);
+
+            if(elem % 2 != 0){
+                minOdd = Math.min(minOdd , elem);
+            }
+            else{
+                minEven = Math.min(minEven , elem);
+
+            }
+        }
+        if(minOdd == Integer.MAX_VALUE || minEven == Integer.MAX_VALUE){
+            return true;
+        }
+
+        boolean allEven = true;
+        int n = nums1.length;
+
+        for (int i = 0; i < n; i++) {
+
+            int elem = nums1[i];
+
+            if (elem % 2 == 0) {
+                continue;
+            }
+
+            boolean byOp2 = false;
+
+            if(elem - minOdd >= 1){
+                byOp2 = true;
+            }
+
+            if (byOp2 == false) {
+                allEven = false;
+                break;
+            }
+        }
+
+        boolean allOdd = true;
+        for (int i = 0; i < n; i++) {
+
+            int elem = nums1[i];
+
+            if (elem % 2 != 0) {
+                continue;
+            }
+
+            boolean byOp2 = false;
+
+            if(elem - minOdd >= 1){
+                byOp2 = true;
+
+            }
+            if (byOp2 == false) {
+                allOdd = false;
+                break;
+            }
+        }
+        return allEven || allOdd;
+
+
+    }
+}
