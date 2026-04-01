7622269d4565545e6050ae7a0e45ff63fce82dcb
Keshav_Verma <144476724+keshav-v2004@users.noreply.github.com>
2026-04-01 22:29:53 +0530

    Time: 30 ms (72.30%) | Memory: 143.7 MB (83.09%) - LeetSync

---
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        ArrayList<Integer> answer = new ArrayList<>();
+
+        int l = 0;
+        int r = 0;
+        int n = nums.length;
+
+        ArrayDeque<Integer> adq = new ArrayDeque<>();
+
+        while(r < k){
+
+            while(!adq.isEmpty() && nums[adq.peekLast()] <= nums[r]){
+                adq.pollLast();
+            }
+            adq.offerLast(r);
+            r++;
+        }
+        answer.add(nums[adq.peekFirst()]);
+        
+        while(r < n){
+
+            // remove out of window elements
+            while( !adq.isEmpty() && adq.peekFirst() <= r-k){
+                adq.pollFirst();
+            }
+            
+            // Maintain monotonic deque
+            while(!adq.isEmpty() && nums[adq.peekLast()] <= nums[r]){
+                adq.pollLast();
+            }
+
+            adq.offerLast(r);
+
+            answer.add(nums[adq.peekFirst()]);
+            r++;
+            l++;
+
+        }
+
+        int[] output = new int[answer.size()];
+        for(int i = 0 ; i < answer.size() ; i++){
+            output[i] = answer.get(i);
+        }
+        return output;
+    }
+}
