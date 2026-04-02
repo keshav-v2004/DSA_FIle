72153e8c7ca8d0ce8839c73be8147a847dcfcc1f
Keshav_Verma <144476724+keshav-v2004@users.noreply.github.com>
2026-04-02 18:45:40 +0530

    Time: 3 ms (1.76%) | Memory: 44.6 MB (13.42%) - LeetSync

---
+++ 78-subsets/subsets.java
@@ -0,0 +1,34 @@
+class Solution {
+
+    List<List<Integer>> answer = new ArrayList<>();
+    Set<List<Integer>> set = new HashSet<>();
+
+    public List<List<Integer>> subsets(int[] nums) {
+        Arrays.sort(nums);
+
+        helper(0,nums,new ArrayList<>());
+
+        return answer;
+    }
+
+    public void helper(int index , int[] nums , List<Integer> each){
+
+        if(index == nums.length){
+            if(!set.contains(each)){
+                answer.add(new ArrayList<>(each));
+                set.add(each);    
+            }
+            return;
+        }
+        
+        // pick this
+        each.add(nums[index]);
+        helper(index+1 , nums , each);
+        each.remove(each.size()-1);
+
+
+        // leave this
+        helper(index+1 , nums ,each);
+
+    }
+}
