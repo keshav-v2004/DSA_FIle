68916c1cc873233322d0e6ce39c5f8f42cab2de3
Keshav_Verma <144476724+keshav-v2004@users.noreply.github.com>
2026-04-02 19:09:33 +0530

    Time: 0 ms (100.00%) | Memory: 45.8 MB (13.44%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,36 +1,38 @@
 class Solution {
+
+    List<List<Integer>> answer = new ArrayList<>();
+
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> answer = new ArrayList<>();
-        List<Integer> each = new ArrayList<>();
-
-        Map<Integer , Boolean> map = new HashMap<>();
-        for(int elem : nums){
-            map.put(elem , false);
-        }
-
-
-        helper(each , map ,nums , answer);
+        helper(0, nums);
         return answer;
     }
 
-    public void helper(List<Integer> each , Map<Integer , Boolean> map , int[] nums , List<List<Integer>> answer){
-        boolean found = false;
+    public void helper(int index , int[] nums ){
 
-        for(int i = 0 ; i< nums.length ; i++){
-            if(map.get(nums[i])==false){
-                found = true;
-                each.add(nums[i]);
-                map.put(nums[i] , true);
-
-                helper(each , map , nums , answer);
-                each.remove(each.size()-1);
-                map.put(nums[i] , false);
+        if(index == nums.length){
+            ArrayList<Integer> each = new ArrayList<>();
+            for(int elem : nums){
+                each.add(elem);
             }
-        }
-        
-        if(found==false){
-            answer.add(new ArrayList<>(each));
+            answer.add(each);
             return;
         }
+
+        for(int i = index ; i < nums.length ; i++){
+
+            // swap index and i
+            swap(nums, index, i);
+
+            helper(index + 1, nums);
+
+            // backtrack
+            swap(nums, index, i);
+        }
+    }
+
+    private void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
